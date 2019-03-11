cb7d77cebd9a27515d8a59b9530ffe10da52a4f6
tilhom <ilhomteshaboyev@gmail.com>
2019-03-11 16:01:40 +0500

    l32-add-into-users-migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,23 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('parent_id')->unsigned()->default(0);
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone');
+            $table->integer('country');
+            $table->integer('state');
+            $table->string('city');
+            $table->text('address');
+            $table->string('post_code');
+            $table->string('logo');
+            $table->enum('status',['off','on'])->deafult('off');
+            $table->integer('company_id')->unsigned()->nullable();
+            $table->integer('role')->unsigned()->default(1);
+            $table->text('access')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
